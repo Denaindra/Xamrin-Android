f598163b62f77a229f056e52c9aafd41e4ac68d8
mgdperera <mgdperera@gmail.com>
2019-02-09 22:13:12 +0530

    GP_add cutome navigation bar

--- AndroidtestUI/Resources/Activities/MainActivity.cs
+++ AndroidtestUI/Resources/Activities/MainActivity.cs
@@ -17,7 +17,6 @@ namespace AndroidtestUI
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             registerUIElements();
-
         }
 
         private void registerUIElements() {
---
+++ AndroidtestUI/Resources/Activities/SecondLayoutActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace AndroidtestUI.Resources
+{
+    [Activity(Label = "SecondLayoutActivity")]
+    public class SecondLayoutActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SecondLayout);
+
+            // Create your application here
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "Custom Toolbar";
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home)
+                Finish();
+
+            return base.OnOptionsItemSelected(item);
+
+        }
+    }
+}
